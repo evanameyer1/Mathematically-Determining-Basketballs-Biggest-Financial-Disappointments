9daf680f1d2efd7ec38ea10989c0973b4bc4b385
evanmeyer <evanameyer1@gmail.com>
2023-03-06 19:50:23 -0600

    finalized initial dashboard

--- queries/final season-by-season scores.sql
+++ queries/final season-by-season scores.sql
@@ -11,8 +11,23 @@ select
 	play.salary,
 	play.percent_of_team,
 	play.wins_contributed,
-	play.production_score
+	play.production_score,
+	(case when play.pos is null then season.pos else play.pos end) as position
 	from player_scores as play
 	join season_totals as season
 		on play.uuid = season.uuid and play.year = season.year
+	where salary != 26238422
 )
+
+drop table player_scores
+drop table season_totals
+
+drop table clean_final_scores
+create table clean_final_scores as (
+select row_number() over (order by production_score desc) as bad_rank, row_number() over (order by production_score asc) as good_rank, *
+from final_seasons_scores
+)
+
+select * from final_seasons_scores where year = 2021 and player = 'Kemba Walker'
+
+select * from season_totals_1996 order by pts desc
